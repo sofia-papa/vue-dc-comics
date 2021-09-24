b0da7f1c08f9196df243a69ebf7007c6ebb2ad6e
sofia-papa <papa.sofia@outlook.it>
2021-09-24 17:00:34 +0100

    header list item

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,21 @@
               <a href="">
                   <img src="../assets/images/dc-logo.png" alt="">
               </a>
+
+              <nav>
+                  <ul>
+                      <li><a href="#">Characters</a></li>
+                      <li><a href="#">Comics</a></li>
+                      <li><a href="#">Movies</a></li>
+                      <li><a href="#">Tv</a></li>
+                      <li><a href="#">Games</a></li>
+                      <li><a href="#">Collectibles</a></li>
+                      <li><a href="#">Videos</a></li>
+                      <li><a href="#">Fans</a></li>
+                      <li><a href="#">News</a></li>
+                      <li><a href="#">Shop</a></li>
+                  </ul>
+              </nav>
           </header>
       </div>
 </template>
@@ -16,5 +31,21 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style/variables.scss";
+
+a img{
+    display: flex;
+}
+
+ul{
+    display: flex;
+    justify-content: center;
+    list-style: none;
+}
+
+li a {
+    padding: 20px;
+    text-decoration: none;
+    
+}
 
 </style>
